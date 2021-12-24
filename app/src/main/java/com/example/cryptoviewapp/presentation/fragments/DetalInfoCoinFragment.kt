a9591182ecf8616f53f7e17e1deab36e0be2cd05
ra1nx92 <ra1nx1992@gmail.com>
2021-12-24 13:30:33 +0300

    adding DI from using koin

--- app/src/main/java/com/example/cryptoviewapp/presentation/fragments/DetalInfoCoinFragment.kt
+++ app/src/main/java/com/example/cryptoviewapp/presentation/fragments/DetalInfoCoinFragment.kt
@@ -7,12 +7,6 @@ import androidx.navigation.fragment.navArgs
 import com.example.cryptoviewapp.R
 import com.example.cryptoviewapp.databinding.FragmentDetalInfoCoinBinding
 import com.example.cryptoviewapp.presentation.viewmodels.CoinInfoViewModel
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import com.github.mikephil.charting.utils.ColorTemplate
 import com.squareup.picasso.Picasso
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -28,7 +22,7 @@ class DetalInfoCoinFragment : Fragment(R.layout.fragment_detal_info_coin) {
         //принимаем данные args.coinPriseInfo
         viewModel.detailInfo(args.coinPriseInfo).observe(viewLifecycleOwner, {
             //устанавливаем данные
-            with(binding){
+            with(binding) {
                 tvPrice.text = it.price.toString()
                 tvMinPrice.text = it.lowday.toString()
                 tvMaxPrice.text = it.highday.toString()
@@ -40,25 +34,5 @@ class DetalInfoCoinFragment : Fragment(R.layout.fragment_detal_info_coin) {
                 Picasso.get().load(it.imageurl).into(ivLogoCoin)
             }
         })
-
-        val barEntry = arrayListOf<BarEntry>()
-        val arabaModel = arrayOf("qwer","qwer2","qwer3","qwer4")
-
-        barEntry.add(BarEntry(0f,50.16f))
-        barEntry.add(BarEntry(1f,32.16f))
-        barEntry.add(BarEntry(2f,22.16f))
-        barEntry.add(BarEntry(3f,12.16f))
-
-        val barDataSet = BarDataSet(barEntry,"AAAAAAAAA")
-        barDataSet.valueTextSize = 15f
-        barDataSet.setColors(*ColorTemplate.MATERIAL_COLORS)
-
-        val barData = BarData(barDataSet)
-        binding.barChart.animateY(1500)
-        binding.barChart.setFitBars(true)
-        binding.barChart.data = barData
-        binding.barChart.description.text = "QQQQQQQQQQQ"
-        binding.barChart.xAxis.valueFormatter = IndexAxisValueFormatter(arabaModel)
-        binding.barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
     }
 }
